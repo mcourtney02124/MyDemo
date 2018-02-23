b6474e28bbfb87755acbe03458cf0d49b3aaa1b4
Meredith <Meredith@10.1.10.19>
2018-02-23 16:33:54 -0500

    start adding logging

---
+++ src/my_log.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+'''
+Make logs for the toy IVR
+Created on Feb 23, 2018
+
+@author: Meredith Courtney
+'''
+import time
+
+class MyLog:
+    '''
+    Make a log file and write to it.
+    '''
+
+    def __init__(self, filepath = "data/test.log"):
+        '''
+        Constructor for the transaction log.
+        '''
+        self.filepath = filepath
+        self.logfile = open(filepath, "w", encoding = "utf08")
+    
+    def log_line(self, id, data):
+        """
+        Make a string of id (timestamp), the timestamp's readable form, and data, and write it to the specified log
+        """
+        self.logfile.write(id + "," + time.ctime(id) + "," + data + "\n")
+        
